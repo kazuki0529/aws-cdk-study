9394fe14faef29e28f793dedceeacbde3bca5620
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2024-10-10 03:29:09 +0000

    chore: #1 Stackのテストコードを追加

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,8 +4,13 @@ import { MyStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new MyStack(app, 'hoge');
 
   const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs18.x',
+  });
+
   expect(template.toJSON()).toMatchSnapshot();
 });
