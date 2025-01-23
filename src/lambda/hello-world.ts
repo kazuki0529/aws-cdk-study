5c049fed19f81ac0230955b7e5a6aa254d600824
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2025-01-23 17:47:31 +0000

    feat: エラー検知用のメトリクスフィルターとアラームを追加

--- src/lambda/hello-world.ts
+++ src/lambda/hello-world.ts
@@ -4,8 +4,10 @@ export const handler: any = async (event: any, context: Context) => {
   console.log('Hello, world!');
   console.log('event:', event);
   console.log('context:', context);
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello, world!'),
-  };
+  // NOTE:エラー発報用にわざとエラーを発生させる
+  throw new Error('This is an error');
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify('Hello, world!'),
+  // };
 };
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,54 @@
 import path from 'path';
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { ComparisonOperator, Metric, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { FilterPattern, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    new NodejsFunction(this, 'hello-world', {
+    const func = new NodejsFunction(this, 'hello-world', {
       entry: path.join(__dirname, 'lambda/hello-world.ts'),
       handler: 'handler',
       runtime: Runtime.NODEJS_LATEST,
       logRetention: RetentionDays.THREE_MONTHS,
     });
+
+    // エラー検知用のメトリクスフィルターを追加
+    const alertTopic = new Topic(this, 'alert-topic', {
+      fifo: false,
+    });
+    alertTopic.addSubscription(new EmailSubscription('xxxxx@email.com'));
+
+    const metricNamespace = 'LogMetrics';
+    const metricName = 'Error';
+    func.logGroup.addMetricFilter('error-metric-filter', {
+      metricNamespace,
+      metricName,
+      filterPattern: FilterPattern.literal('ERROR'),
+    });
+
+    const metric = new Metric({
+      namespace: metricNamespace,
+      metricName,
+      period: Duration.minutes(1),
+      statistic: 'Sum',
+    });
+
+    metric.createAlarm(this, 'error-alarm', {
+      alarmName: 'error-alarm',
+      evaluationPeriods: 1,
+      threshold: 1,
+      treatMissingData: TreatMissingData.NOT_BREACHING,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      actionsEnabled: true,
+    }).addAlarmAction(new SnsAction(alertTopic));
   }
 }
 
